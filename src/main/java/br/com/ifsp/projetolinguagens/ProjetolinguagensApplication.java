9476c672f02eb2488b6a4aa2fadc533f8b5bf694
Tamiris-Jesus <103379387+Tamiris-Jesus@users.noreply.github.com>
2023-04-24 17:51:41 -0300

    Commit intial

---
+++ src/main/java/br/com/ifsp/projetolinguagens/ProjetolinguagensApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifsp.projetolinguagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetolinguagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetolinguagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifsp/projetolinguagens/ProjetolinguagensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifsp.projetolinguagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetolinguagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
